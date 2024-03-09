ab6f43963d8c41639c667b0ffa0a4757daadd2f4
diana <diana@dianath>
2024-03-09 06:27:25 -0600

    Actualización de repositorio

---
+++ src/main/java/com/mycompany/playlist/Playlist.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.playlist;
+
+/**
+ *
+ * @author diana
+ */
+public class Playlist {
+
+    public static void main(String[] args) {
+     Musicas P1=new Musicas("El paso del gigante","Cumbias Mexicanas","1987",3);
+     P1.ShowPlaylist();
+    }
+}
